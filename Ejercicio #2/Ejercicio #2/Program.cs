f2a6d69c74f745a838f0cd626f943744f339c89b
achitoo <hbaltodano@uamv.edu.ni>
2024-08-22 17:49:46 -0600

    Ejercicio #1 y 2

---
+++ Ejercicio #2/Ejercicio #2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ejercicio__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de variables;
+            string nombre;
+            //Leer el nombre y saludar
+            Console.WriteLine("ola!");
+            Console.Write("Digite su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + ", mucho gusto!");
+
+        }
+    }
+}
