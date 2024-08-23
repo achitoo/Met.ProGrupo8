2a48a4095816cb6a9f706dae16c49d7ff89f64e6
achitoo <hbaltodano@uamv.edu.ni>
2024-08-22 18:29:06 -0600

    Ejercicio #4

---
+++ Ejercicio #4/Ejercicio #4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad, result;
+            Console.WriteLine("Calculo del area de un circulo");
+            Console.WriteLine("Ingrese el radio del circulo");
+            rad=double.Parse(Console.ReadLine());
+            
+            result= Math.PI*Math.Pow(2,rad);
+            Console.WriteLine("El area del circulo es " + result);
+
+            
+        }
+    }
+}
