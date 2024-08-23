f63a7b64eb3e642be12892fed85df3a7943c8493
achitoo <hbaltodano@uamv.edu.ni>
2024-08-22 18:08:24 -0600

    Ejercicio #3

---
+++ Ejercicio #3/Ejercicio #3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ejercicio__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, result;
+            Console.WriteLine("Suma de dos numeros");
+            Console.WriteLine("Ingrese primer numero entero");
+            num1= float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero entero");
+            num2= float.Parse(Console.ReadLine());
+
+            result = num1 + num2;
+            Console.WriteLine("El resultado es " + result);
+            
+        }
+    }
+}
