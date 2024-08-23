bf8870315a9c76c7ad7af18f98e1983260d7fc5e
achitoo <hbaltodano@uamv.edu.ni>
2024-08-22 18:48:40 -0600

    Ejercicio

---
+++ Ejercico #5/Ejercico #5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercico__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, cargo;
+            double salr, result;
+            
+            Console.WriteLine("Bienvenido");
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su cargo");
+            cargo = Console.ReadLine();
+            Console.WriteLine("Digite su salario sin comas");
+            salr = double.Parse(Console.ReadLine());
+            result = salr*1.10;
+            Console.WriteLine("Su nombre es: " + nombre );
+            Console.WriteLine("Su cargo es: " + cargo );
+            Console.WriteLine("Su salario mas el 10 por ciento aplicado es: "+ result);
+        }
+    }
+}
